881ac23d2e870ff9ce1a515681fd134b75530772
Marta109 <marta.hayrapetyan.a@gmail.com>
2023-02-16 16:34:08 +0400

    project 1 completed all

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # word-counter-Marta-Hayrapetyan
+What is WordCounter?
+It's a simple word counter, you need to type words in the field, at the bottom you can find out in real time how many sentences you have already typed, the number of digits, the number of special characters called  Symbols, the number of letters, the number of all characters and the number of characters without spaces, as well as the number of words. The counter reads the text as words if it contains at least one letter, and also warns if the new sentence starts with a small letter
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const changeColorBtn = document.querySelector(".btn-primary"),
-  calculateBtn = document.querySelector(".calculate"),
-  alertCaluletedValue = document.getElementById("liveAlertCaluletedValue");
+  textarea = document.querySelector(".textarea");
 
 changeColorBtn,
   addEventListener(
@@ -15,67 +14,82 @@ changeColorBtn,
   );
 
 function genretRandomColor() {
-  var letters = "0123456789ABCDEF";
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+  const chars = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += chars[Math.floor(Math.random() * 16)];
   }
   return color;
 }
 
-calculateBtn.addEventListener("click", (e) => {
-  const textarea = document.querySelector(".textarea");
-  let str = textarea.value;
-  if (str.length === 0 || str.replace(/\s/g, "").length === 0) {
-    alert("Incorrectly entered data", "danger");
-  } else {
-    textCounter(str);
-  }
+textarea.addEventListener("input", (e) => {
+  textCounter(e.target.value);
 });
 
-function alert(message, type) {
-  const wrapper = document.createElement("div");
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    "</div>",
-  ].join("");
-  alertCaluletedValue.append(wrapper);
+function textCounter(str) {
+  const charWithoutSpace = document.querySelector(".charWithoutSpace"),
+    chars = document.querySelector(".chars"),
+    words = document.querySelector(".words"),
+    sentences = document.querySelector(".sentences"),
+    numbers = document.querySelector(".numbers"),
+    symbols = document.querySelector(".symbols"),
+    letters = document.querySelector(".letters");
+
+  chars.innerHTML = str.length;
+  sentences.innerHTML = sentenceCounter(str);
+  words.innerHTML = wordCounter(str);
+  letters.innerHTML = letterCounter(str);
+  numbers.innerHTML = str.replace(/[^0-9]/g, "").length;
+  charWithoutSpace.innerHTML = str.replace(/\s/g, "").length;
+  symbols.innerHTML = str.replace(/[a-zа-яё0-9\s]/gi, "").length;
 }
 
-function textCounter(str) {
-  let wordCounty = 0,
-    letterCounty = 0,
-    numberCounty = 0;
-
+function letterCounter(str) {
+  let letterCounty = 0;
+  str = str.replace(/[^a-zа-яё\s]/gi, "").split("");
   for (let i = 0; i < str.length; i++) {
-    str[i] != " " && isNaN(str[i])
-      ? letterCounty++
-      : str[i] != " " && !isNaN(str[i])
-      ? numberCounty++
-      : "";
+    str[i] != " " && isNaN(str[i]) ? letterCounty++ : "";
   }
-  let strArr = str
+
+  return letterCounty;
+}
+function wordCounter(str) {
+  let wordCounty = 0;
+  str
     .split(" ")
     .join("!!")
     .split(",")
     .join("!!")
     .split(".")
     .join("!!")
-    .split("!!");
-  console.log(strArr);
-  strArr.forEach((el) => {
-    if (el.length >= 2 && isNaN(el)) {
-      wordCounty++;
-    }
-  });
-
-  alert(
-    ` string is "${str}" <br>
-      number of letters = ${letterCounty} <br>
-      number of words = ${wordCounty} <br>
-      number of numbers = ${numberCounty}`,
-    "success"
-  );
+    .split("!!")
+    .forEach((el) => {
+      if (el.length >= 1 && isNaN(el)) {
+        wordCounty++;
+      }
+    });
+  return wordCounty;
 }
+
+function sentenceCounter(str) {
+  validateText(str);
+  return str.split(/[.!?]+\s/).filter(Boolean).length;
+}
+
+const validateText = (str) => {
+  const errorText = document.querySelector(".error-text");
+  let strArr = str.split(/[.!?]+\s/).filter(Boolean);
+  console.log(strArr);
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i] !== "") {
+      strArr[i][0] == strArr[i][0].toUpperCase()
+        ? (errorText.textContent = "")
+        : (errorText.textContent =
+            "New sentence must start with a capital letter");
+    }
+  }
+
+  if (strArr.length == 0) {
+    errorText.textContent = "";
+  }
+};
